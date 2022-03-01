6adfb53127ba260776b4b034b22378ef7bcac6eb
Weikai Kong <weikaikong@yeah.net>
2022-03-01 17:34:23 +0800

    [master] initial upload data_preprocessing.py

---
+++ data_preprocessing.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+import torch
+
+# create datasets, store in .csv
+os.makedirs(os.path.join('..','data'),exist_ok=True)
+data_file = os.path.join('..', 'data', 'house_tiny.csv')
+with open(data_file, 'w') as f:
+    f.write('NumRooms,Alley,Price\n')  # 列名
+    f.write('NA,Pave,127500\n')  # 每行表示一个数据样本
+    f.write('2,NA,106000\n')
+    f.write('4,NA,178100\n')
+    f.write('NA,NA,140000\n')
+
+# load datasets from .csv
+data = pd.read_csv(data_file)
+print(data)
+
+# for NaN data, usually delete or interpolate
+# interpolation
+# iloc = index location
+inputs, outputs = data.iloc[:,0:2], data.iloc[:,2]
+# fillna = fill NaN
+inputs = inputs.fillna(inputs.mean())
+print(inputs)
+
+# for which type not index, see 'NaN' as a class
+inputs = pd.get_dummies(inputs, dummy_na=True)
+print(inputs)
+
+# All inputs and outputs are integer -> transfer them to tensor
+x, y = torch.tensor(inputs.values), torch.tensor(outputs.values)
+print(x,y)
